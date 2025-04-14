2421585bfffb6652f7f85f5c8832aaca3c05e99b
Z1Colo <nicolouw44@gmail.com>
2025-04-14 13:36:10 +0700

    Title screen
    - Added title text
    - Added start button to go from Title screen to Main screen
    - Added script to switch between Title and main screen

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main"); // replace with your actual scene name
+    }
+}
+
